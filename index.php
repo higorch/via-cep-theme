f2653516cff37dd916d8be9ac35b35cf83ed527f
Higor Christian <higorchristianfe@gmail.com>
2022-07-28 00:04:23 -0300

    frontend zip box

--- index.php
+++ index.php
@@ -12,6 +12,27 @@
 <body>
     <?php wp_body_open(); ?>
 
+    <div class="zip-box">
+
+        <div class="pin">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/location.png">
+        </div>
+
+        <div class="heading">
+            <h2>Consulte o endereço pelo CEP</h2>
+        </div>
+
+        <div class="entry">
+            <label for="zip">Cep</label>
+            <div class="group">
+                <input type="text" id="zip" placeholder="01001000">
+                <button type="button">Consultar</button>
+            </div>
+        </div>
+
+    </div>
+    <!-- zip-box -->
+
     <?php wp_footer(); ?>
 </body>
 
