b96571a6792cf898a289c46ef9bb225675fda1d8
Higor Christian <higorchristianfe@gmail.com>
2022-07-27 22:47:56 -0300

    first commit

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+require_once get_template_directory() . '/includes/class-theme-setup.php';
+require_once get_template_directory() . '/includes/class-theme-cpt.php';
---
+++ includes/class-theme-setup.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class ThemeSetup
+{
+    public function __construct()
+    {
+        add_action('wp_enqueue_scripts', [$this, 'enqueue_scripts']);
+    }
+
+    public function enqueue_scripts()
+    {
+        wp_enqueue_style('fonts', get_template_directory_uri() . '/assets/fonts/stylesheet.css', [], '1.0.0');
+        wp_enqueue_style('theme', get_template_directory_uri() . '/assets/css/theme.css', [], '1.0.0');
+        
+        wp_enqueue_script('theme', get_template_directory_uri() . '/assets/js/theme.js', ['jquery'], '1.0.0', true);
+    }
+}
+
+new ThemeSetup();
