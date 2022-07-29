43dbe161596d9e9453d0eaa1509af195564846be
Higor Christian <higorchristianfe@gmail.com>
2022-07-29 11:50:40 -0300

    login custom

--- includes/class-theme-setup.php
+++ includes/class-theme-setup.php
@@ -9,6 +9,7 @@ class ThemeSetup
     public function __construct()
     {
         add_action('wp_enqueue_scripts', [$this, 'enqueue_scripts']);
+        add_action('login_enqueue_scripts', [$this, 'custom_login_style']);
     }
 
     public function enqueue_scripts()
@@ -21,6 +22,11 @@ class ThemeSetup
         wp_enqueue_script('jquery-mask', get_template_directory_uri() . '/assets/plugins/jquery-mask/jquery.mask.min.js', ['jquery'], null, true);
         wp_enqueue_script('cute-alert', get_template_directory_uri() . '/assets/plugins/cute-alert/cute-alert.js', ['jquery'], null, true);
         wp_enqueue_script('theme', get_template_directory_uri() . '/assets/js/theme.js', ['jquery'], '1.0.0', true);
+    }
+
+    public function custom_login_style()
+    {
+        wp_enqueue_style('login-style', get_template_directory_uri() . '/assets/css/login.css', null, '8.6.2');
     }
 }
 
