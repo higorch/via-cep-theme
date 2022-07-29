e2b9c3d0d7a64982cb59f1df01b9fd765f93e0ea
Higor Christian <higorchristianfe@gmail.com>
2022-07-28 23:45:40 -0300

    frontend

--- index.php
+++ index.php
@@ -1,39 +1,44 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+<?php get_header(); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title(); ?></title>
-    <?php wp_head(); ?>
-</head>
+<div class="zip-box">
 
-<body>
-    <?php wp_body_open(); ?>
+    <div class="pin">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/location.png">
+    </div>
 
-    <div class="zip-box">
+    <div class="heading">
+        <h2>Consulte o endereço pelo CEP</h2>
+    </div>
 
-        <div class="pin">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/location.png">
+    <div class="entry">
+        <label for="zip">Cep</label>
+        <div class="group">
+            <input type="text" id="zip" placeholder="01001000">
+            <button type="button">Consultar</button>
         </div>
+    </div>
 
-        <div class="heading">
-            <h2>Consulte o endereço pelo CEP</h2>
-        </div>
-
-        <div class="entry">
-            <label for="zip">Cep</label>
-            <div class="group">
-                <input type="text" id="zip" placeholder="01001000">
-                <button type="button">Consultar</button>
+    <div class="show-address active">
+        <div class="head">
+            <div class="col">
+                <b>Endereço</b>
+            </div>
+            <div class="col">
+                <a href="#" title="Salvar">
+                    <i class="fa-solid fa-floppy-disk"></i>
+                </a>
             </div>
         </div>
-
+        <p>BR-153, 2514 - Vila Santa, Aparecida de Goiânia - GO, 74910-180</p>
     </div>
-    <!-- zip-box -->
 
-    <?php wp_footer(); ?>
-</body>
+</div>
+<!-- zip-box -->
 
-</html>
+<?php get_template_part('templates/saved', 'address'); ?>
+
+<?php
+
+get_footer();
---
+++ templates/saved-address.php
@@ -0,0 +1,27 @@
+<div class="address-box">
+
+    <!-- <div class="empty">
+        <div class="icon">
+            <i class="fa-regular fa-circle-xmark"></i>
+        </div>
+        <p>Nenhum endereço salvo.</p>
+    </div> -->
+
+    <div class="heading">
+        <h2>Endereços salvos</h2>
+    </div>
+
+    <div class="items">
+
+        <ul>
+            <li>BR-153, 2514 - Vila Santa, Aparecida de Goiânia - GO, 74910-180</li>
+            <li>BR-153, 2514 - Vila Santa, Aparecida de Goiânia - GO, 74910-180</li>
+            <li>BR-153, 2514 - Vila Santa, Aparecida de Goiânia - GO, 74910-180</li>
+            <li>BR-153, 2514 - Vila Santa, Aparecida de Goiânia - GO, 74910-180</li>
+            <li>BR-153, 2514 - Vila Santa, Aparecida de Goiânia - GO, 74910-180</li>
+        </ul>
+
+    </div>
+
+</div>
+<!-- address-box -->
